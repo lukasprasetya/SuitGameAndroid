31ee0cb005b8caaeb5c02258bf5bf47272b5cb74
Lukas <lukasprasetya1208@gmail.com>
2022-09-25 16:39:01 +0700

    Finishing Player Character

--- app/src/main/java/com/lupa/suitgameandroid/MainActivity.kt
+++ app/src/main/java/com/lupa/suitgameandroid/MainActivity.kt
@@ -2,11 +2,35 @@ package com.lupa.suitgameandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.lupa.suitgameandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.apply {
+            flRock.setOnClickListener(View.OnClickListener {
+                binding.ivPlayer.setImageResource(R.drawable.ic_left_rock)
+
+            })
+
+            flPaper.setOnClickListener(View.OnClickListener {
+                binding.ivPlayer.setImageResource(R.drawable.ic_left_paper)
+
+            })
+
+            flScissor.setOnClickListener(View.OnClickListener {
+                binding.ivPlayer.setImageResource(R.drawable.ic_left_scissor)
+
+            })
+        }
     }
 }
